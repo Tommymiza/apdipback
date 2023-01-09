6f6c715bcd921c9ca7e8274f23774bb67deee530
Tommymiza <tommymiza20@gmail.com>
2023-01-09 15:39:38 +0300

    cors

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const port = 4422;
 const fileUpload = require("express-fileupload");
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload({ createParentPath: true }));
