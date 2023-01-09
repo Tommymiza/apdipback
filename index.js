b99d1cc07fdeb2ce89803d797df10864c47003d2
Tommymiza <tommymiza20@gmail.com>
2023-01-09 16:04:54 +0300

    Header

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ const app = express();
 
 app.use(cors({
   origin: "*",
-  methods: ["POST","GET"]
+  methods: ["POST","GET"],
 }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload({ createParentPath: true }));
